16a8cba8785488d21065de8644cf639cbd1af1fe
Haonter <101908524+Haonter@users.noreply.github.com>
2023-04-11 20:09:43 -0400

    JavaScript

---
+++ JavaScript/Seccion_3/Eventos.js
@@ -0,0 +1,32 @@
+/*
+    ¿Qué son Eventos?
+    Los eventos son la manera en la cual JavaScript controla las acciones de los visitantes en una web 
+    y define el comportamiento de la página cuando estos se produzcan. 
+
+    Cuando un usuario visita una página web e interactúa con ella se producen los eventos y con Javascript 
+    podemos definir qué queremos que ocurra cuando se produzcan ciertos eventos o situaciones.
+
+    Para entender los eventos es necesario conocer algunos conceptos básicos:
+
+    •	Evento: Es algo que ocurre. Generalmente los eventos ocurren cuando el usuario interactúa con el 
+        documento, pero podrían producirse por situaciones ajenas al usuario, como en el caso de que una 
+        imagen no este disponible.
+
+    •	Tipo de evento: Es el tipo del suceso que ha ocurrido, por ejemplo, un clic sobre un botón o
+        el envío de un formulario. Cada tipo de elemento de la página ofrece diversos tipos de eventos 
+        de JavaScript. 
+
+    •	Manejador de evento: Es el comportamiento que nosotros podemos asignar como respuesta a un 
+        evento. Se especifica mediante una función en JavaScript, que se asocia a un tipo de evento en concreto. 
+        Una vez asociado el manejador a un tipo de evento sobre un elemento de la página, y cada vez que 
+        ocurre ese tipo de evento sobre ese elemento en concreto, se ejecutará el manejador de evento asociado.
+
+    Formas de manejar eventos:
+
+    •	Mediante atributos HTML: <button onclick="miFuncion()">Presiona Aquí!</button>
+
+*/
+
+
+
+
