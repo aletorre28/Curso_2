8e7613b9978dc51ea4ef729d95f9ac110d2ed6f1
Haonter <101908524+Haonter@users.noreply.github.com>
2023-07-04 19:10:13 -0400

    Python

---
+++ Python/Seccion_1/Operadores_Comparativos.py
@@ -0,0 +1,15 @@
+# Los operadores comparativos se utilizan para comparar dos valores 
+# y producir un resultado booleano (True o False).
+
+
+# Variables a comparar
+variable1 = 5
+variable2 = 30
+
+
+# >: mayor que
+# <: menor que
+# >=: mayor o igual que
+# <=: menor o igual que
+# ==: igual que
+# !=: distinto de
