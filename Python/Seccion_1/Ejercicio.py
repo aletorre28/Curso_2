c6cc22caf69fed542ec63bb5a217467a5e1380db
Haonter <101908524+Haonter@users.noreply.github.com>
2023-07-06 19:27:29 -0400

    Python

---
+++ Python/Seccion_1/Ejercicio.py
@@ -0,0 +1,19 @@
+"""
+    Escribe un programa en Python que solicite al usuario ingresar el nombre y la edad de tres personas. 
+    El programa debe determinar quién es la persona mayor y mostrar su nombre y edad en pantalla.
+
+    El programa debe realizar lo siguiente:
+
+    1 - Solicitar al usuario que ingrese el nombre y la edad de la primera persona.
+    2 - Solicitar al usuario que ingrese el nombre y la edad de la segunda persona.
+    3 - Solicitar al usuario que ingrese el nombre y la edad de la tercera persona.
+    4 - Utilizar una estructura condicional para comparar las edades y determinar quién es la persona de mayor edad.
+    5 - determinar si dicha persona es mayor de edad.
+    6 - Mostrar en pantalla el nombre y la edad de dicha persona y si es o no mayor de edad.
+
+    Recuerda utilizar comentarios para documentar tu código y explicar qué hace cada parte. 
+    Puedes utilizar variables para almacenar los nombres y edades ingresados por el usuario, y comparar las edades utilizando 
+    operadores de comparación.
+"""
+
+
