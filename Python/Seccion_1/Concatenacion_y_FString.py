c03ee9a98017e945f317e2524e646e3e81e84f36
Haonter <101908524+Haonter@users.noreply.github.com>
2023-07-06 18:53:28 -0400

    Python

--- Python/Seccion_1/Concatenacion_y_FString.py
+++ Python/Seccion_1/Concatenacion_y_FString.py
@@ -6,11 +6,11 @@
 
 # Ejemplo: 
 
-# nombre = input("Ingrese su nombre: ")
-# apellido = input("Ingrese su apellido: ")
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
 
 
-# print("Hola me llamo: " + nombre + " " + apellido)
+print("Hola me llamo: " + nombre + " " + apellido)
 
 
 """
@@ -21,7 +21,7 @@
 """
 
 # Ejemplo: 
-# print(f"Hola, ¿Como estas?, me llamo {nombre} {apellido}")
+print(f"Hola, ¿Como estas?, me llamo {nombre} {apellido}")
 
 
 
@@ -35,6 +35,11 @@ numero2 = float( input("Ingrese el segundo numero: ") )
 print(f"La suma de {numero1} y {numero2} es: {numero1 + numero2}")
 
 
+#  Ejemplo 3:
+numero3 = input("Ingrese un numero: ")
+numero4 = input("Ingrese otro numero: ")
+print(f"La suma de {numero3} y {numero4} es: {float(numero3) + float(numero4)}")
+
 
 
 # Datos primarios
@@ -45,3 +50,24 @@ print(f"La suma de {numero1} y {numero2} es: {numero1 + numero2}")
 # x = tuple("apple", "banana", "cherry")	
 # x = dict({"name" : "John", "age" : 36})
 # x = bool (True) #(Con "T" mayuscula)
+
+
+
+
+# Aporte de Wilklert Garrido
+#Hallar el cociente y el residuo de dos numeros enteros
+print('Hallar el cociente y el residuo de dos numeros enteros')
+
+a = input("Ingrese el primer numero: ")
+b = input("Ingrese el segundo numero: ")
+
+#OPERACION
+a = int(a)
+b = int(b)
+
+cociente = a // b 
+residuo = a % b 
+
+#SALIDA
+print(f"El cociente es: {cociente}")
+print(f"El residuo es: {residuo}")
